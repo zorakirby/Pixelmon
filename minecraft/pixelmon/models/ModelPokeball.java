017350383d77548437ec51be360e7a912efda2b7
MrMasochism <malc.geddes@gmail.com>
2012-07-25 21:59:18 +1200

    little pokeball fixes

--- minecraft/pixelmon/models/ModelPokeball.java
+++ minecraft/pixelmon/models/ModelPokeball.java
@@ -218,14 +218,13 @@ public class ModelPokeball extends ModelBase
 		Hinge.addChild(Button);
   }
   
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
+  public void render(float f)
   {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5);
-    Rocking.render(f5);
+    //setRotationAngles(f, f1, f2, f3, f4, f5);
+    Rocking.render(f);
   }
   
-  private void setRotation(ModelRenderer model, float x, float y, float z)
+  protected void setRotation(ModelRenderer model, float x, float y, float z)
   {
     model.rotateAngleX = x;
     model.rotateAngleY = y;
--- minecraft/pixelmon/render/RenderPokeball.java
+++ minecraft/pixelmon/render/RenderPokeball.java
@@ -36,6 +36,7 @@ public class RenderPokeball extends Render {
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		GL11.glRotatef(180, 1, 0, 1);
 		MinecraftForgeClient.bindTexture("/pixelmon/texture/pokeballs/"+pokeball.type.getTexture());
 		RenderHelper.enableStandardItemLighting();
 		float factor = (float) (1.0/16.0);
