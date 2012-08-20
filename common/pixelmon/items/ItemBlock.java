ed328261256f6162794a9c70b242fa63a3f74c2d
Gerald Wood <xkyouchoux@hotmail.com>
2012-08-20 04:12:02 -0400

    fixed ItemBlock

--- common/pixelmon/items/ItemBlock.java
+++ common/pixelmon/items/ItemBlock.java
@@ -30,47 +30,68 @@ public class ItemBlock extends Item {
 	 */
 	@Override
 	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side) {
-		if (side != 1) {
-			return true;
-		} else {
-			++y;
-			int var6 = MathHelper.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-			int var9 = 0;
-			if (var6 == 0) {
-				var9 = 2;
-			}
+		if (side != 1) 
+		{
+			return false;
+		} 
+		else 
+		{
+			if(!world.isRemote)
+			{
+				++y;
+				int var6 = MathHelper.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+				int var9 = 0;
+				if (var6 == 0) 
+				{
+					var9 = 2;
+				}
 
-			if (var6 == 1) {
-				var9 = 1;
-			}
+				if (var6 == 1)
+				{
+					var9 = 1;
+				}
 
-			if (var6 == 2) {
-				var9 = 0;
-			}
+				if (var6 == 2)
+				{
+					var9 = 0;
+				}
 
-			if (var6 == 3) {
-				var9 = 3;
-			}
-			if (player.canPlayerEdit(x, y, z) && player.canPlayerEdit(x, y + 1, z)) {
-				if (world.isAirBlock(x, y, z) && world.isAirBlock(x, y + 1, z) && world.isBlockNormalCube(x, y - 1, z) && world.getBlockId(x, y - 1, z) != block.blockID) {
-					if(block instanceof BlockPC && world.getBlockId(x, y + 2, z) == block.blockID)
+				if (var6 == 3)
+				{
+					var9 = 3;
+				}
+				if (player.canPlayerEdit(x, y, z) && player.canPlayerEdit(x, y + 1, z)) 
+				{
+					if (world.isAirBlock(x, y, z) && world.isAirBlock(x, y + 1, z) && world.isBlockNormalCube(x, y - 1, z) && world.getBlockId(x, y - 1, z) != block.blockID) 
 					{
+						if(block instanceof BlockPC && world.getBlockId(x, y + 2, z) == block.blockID)
+						{
+							return false;
+						}
+						world.setBlockAndMetadataWithNotify(x, y, z, block.blockID, var9);
+						if (world.getBlockId(x, y, z) == block.blockID && block instanceof BlockPC) 
+						{
+							world.setBlockAndMetadataWithNotify(x, y + 1, z, block.blockID, var9 - 6);
+						}
+						if(!player.capabilities.isCreativeMode)
+						{
+							--stack.stackSize;
+						}
 						return true;
 					}
-					world.setBlockAndMetadataWithNotify(x, y, z, block.blockID, var9);
-					if (world.getBlockId(x, y, z) == block.blockID && block instanceof BlockPC) {
-						world.setBlockAndMetadataWithNotify(x, y + 1, z, block.blockID, var9 - 6);
-					}
-					if(!player.capabilities.isCreativeMode)
+					else 
 					{
-						--stack.stackSize;
+						return false;
 					}
+				} 
+				else
+				{
 					return false;
-				} else {
-					return true;
 				}
-			} else {
-				return true;
+			}
+			else 
+			{
+				return false;
 			}
 		}
 	}
