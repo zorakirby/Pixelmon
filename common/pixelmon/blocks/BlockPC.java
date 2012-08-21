410ee43655a6dfc5ffcd74c7698d49f7f04b70af
MrMasochism <malc.geddes@gmail.com>
2012-08-21 16:24:41 -0700

    fix to blockPC

--- common/pixelmon/blocks/BlockPC.java
+++ common/pixelmon/blocks/BlockPC.java
@@ -3,16 +3,23 @@ package pixelmon.blocks;
 import java.util.Random;
 
 import pixelmon.Pixelmon;
+import pixelmon.comm.EnumPackets;
+import pixelmon.comm.PixelmonDataPacket;
 import pixelmon.config.PixelmonItems;
 import pixelmon.enums.EnumGui;
+import pixelmon.storage.ComputerBox;
+import pixelmon.storage.PixelmonStorage;
+import pixelmon.storage.PlayerComputerStorage;
 import net.minecraft.src.Block;
 import net.minecraft.src.BlockContainer;
 import net.minecraft.src.EntityLiving;
 import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.EntityPlayerMP;
 import net.minecraft.src.IBlockAccess;
 import net.minecraft.src.Material;
 import net.minecraft.src.MathHelper;
 import net.minecraft.src.ModLoader;
+import net.minecraft.src.NBTTagCompound;
 import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
 
@@ -60,7 +67,17 @@ public class BlockPC extends BlockContainer {
 	public boolean onBlockActivated(World world, int par2, int par3, int par4, EntityPlayer player, int par6, float par7, float par8, float par9) {
 		if(!world.isRemote)
 		{
-			player.openGui(Pixelmon.instance, EnumGui.PC.getIndex(), world, 0, 0, 0);
+			PlayerComputerStorage s = PixelmonStorage.ComputerManager.getPlayerStorage(player);
+			for(ComputerBox b : s.getBoxList()){
+				for(NBTTagCompound n: b.getStoredPokemon()){
+					if (n != null) {
+						PixelmonDataPacket p = new PixelmonDataPacket(n, EnumPackets.AddToTempStore);
+						((EntityPlayerMP)player).serverForThisPlayer.sendPacketToPlayer(p.getPacket());
+					}
+				}
+			}
+			player.openGui(Pixelmon.instance, EnumGui.PC.getIndex(), world, 0,0,0); // GUIPC
+			return true;
 		}
 		return true;
 	}
