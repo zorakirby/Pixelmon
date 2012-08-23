48dd1f6806d64a2950b8e4b8801e7dce93435473
Garrett <garrettjones331@gmail.com>
2012-08-23 00:58:29 -0400

    Gastly Particle effect Finished

---
+++ common/pixelmon/battles/animations/particles/EntityGastlyParticle.java
@@ -0,0 +1,70 @@
+package pixelmon.battles.animations.particles;
+
+import net.minecraft.src.EntityFX;
+import net.minecraft.src.Tessellator;
+import net.minecraft.src.World;
+
+public class EntityGastlyParticle extends EntityFX
+{
+    float field_70569_a;
+
+    public EntityGastlyParticle(World par1World, double par2, double par4, double par6, double par8, double par10, double par12)
+    {
+        super(par1World, par2, par4, par6, 0.0D, 0.0D, 0.0D);
+        float var14 = 2.5F;
+        this.motionX *= 0.10000000149011612D;
+        this.motionY *= 0.10000000149011612D;
+        this.motionZ *= 0.10000000149011612D;
+        this.motionX += par8;
+        this.motionY += par10;
+        this.motionZ += par12;
+        this.particleRed = 100F;
+        this.particleGreen = 0F;
+        this.particleBlue = 100F;
+        this.particleScale *= .5F;
+        this.particleScale *= var14;
+        this.field_70569_a = this.particleScale;
+        this.particleMaxAge = (int)(8.0D / (Math.random() * 0.8D + 0.3D));
+        this.particleMaxAge = (int)((float)this.particleMaxAge * var14);
+        this.noClip = false;
+    }
+
+    public void renderParticle(Tessellator par1Tessellator, float par2, float par3, float par4, float par5, float par6, float par7)
+    {
+        float var8 = ((float)this.particleAge + par2) / (float)this.particleMaxAge * 32.0F;
+
+        if (var8 < 0.0F)
+        {
+            var8 = 0.0F;
+        }
+
+        if (var8 > 1.0F)
+        {
+            var8 = 1.0F;
+        }
+
+        this.particleScale = this.field_70569_a * var8;
+        super.renderParticle(par1Tessellator, par2, par3, par4, par5, par6, par7);
+    }
+
+    /**
+     * Called to update the entity's position/logic.
+     */
+    public void onUpdate()
+    {
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
+
+        if (this.particleAge++ >= this.particleMaxAge)
+        {
+            this.setDead();
+        }
+
+        this.setParticleTextureIndex(7 - this.particleAge * 8 / this.particleMaxAge);
+        this.motionX *= 0.9599999785423279D;
+        this.motionY *= 0.9599999785423279D;
+        this.motionZ *= 0.9599999785423279D;
+
+    }
+}
