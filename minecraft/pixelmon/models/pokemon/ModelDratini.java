2925847bb2aa960ace8744d37e1a0e64c642963e
Garrett <garrettjones331@gmail.com>
2012-08-26 01:28:00 -0400

    Weezing particle effect added and dratini head turning added (can't bee seen in game yet though)

--- minecraft/pixelmon/models/pokemon/ModelDratini.java
+++ minecraft/pixelmon/models/pokemon/ModelDratini.java
@@ -382,7 +382,7 @@ public class ModelDratini extends ModelBase
   public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
   {
     super.setRotationAngles(f, f1, f2, f3, f4, f5);
- 
+   
   }
 
 }
