61df0adfd917e804e32802de1c2eac35e09174aa
MrMasochism <malc.geddes@gmail.com>
2012-09-02 21:44:33 -0700

    added DropItemHelper

--- common/pixelmon/entities/pixelmon/helpers/DropItemHelper.java
+++ common/pixelmon/entities/pixelmon/helpers/DropItemHelper.java
@@ -1,8 +1,15 @@
 package pixelmon.entities.pixelmon.helpers;
 
+import pixelmon.entities.pixelmon.Entity8HoldsItems;
 import net.minecraft.src.Item;
 
 public class DropItemHelper {
+
+	Entity8HoldsItems pixelmon;
+	
+	public DropItemHelper(Entity8HoldsItems entity8HoldsItems) {
+		pixelmon = entity8HoldsItems;
+	}
 
 	public int getDropItemID() {
 		return Item.bone.shiftedIndex;
