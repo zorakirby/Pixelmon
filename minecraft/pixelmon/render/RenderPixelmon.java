36a4b747d12b8fde0a2f8c893dc35331b65a5e8a
MrMasochism <malc.geddes@gmail.com>
2012-09-08 15:01:24 -0700

    trainer's pokemon now show trainer's name as owner instead of wild

--- minecraft/pixelmon/render/RenderPixelmon.java
+++ minecraft/pixelmon/render/RenderPixelmon.java
@@ -52,8 +52,8 @@ public class RenderPixelmon extends RenderLiving {
 			s += entitypixelmon.getNickname();
 			if (entitypixelmon.getOwner() != null) {
 				s += " (" + ((EntityPlayer) entitypixelmon.getOwner()).username + ")";
-			} else {
-				s += " (Wild)";
+			} else if (entitypixelmon.trainer != null) {
+				s += " (" + entitypixelmon.trainer.info.name + ")";
 			}
 			if (!entitypixelmon.isSneaking()) {
 				renderLivingLabel(entitypixelmon, s, par2, par4, par6, 64);
@@ -61,50 +61,48 @@ public class RenderPixelmon extends RenderLiving {
 		}
 	}
 
-	 protected void renderLivingLabel(EntityLiving entityLiving, String par2Str, double par3, double par5, double par7, int par9)
-	    {
-	        double var10 = entityLiving.getDistanceSqToEntity(this.renderManager.livingPlayer);
+	protected void renderLivingLabel(EntityLiving entityLiving, String par2Str, double par3, double par5, double par7, int par9) {
+		double var10 = entityLiving.getDistanceSqToEntity(this.renderManager.livingPlayer);
 
-	        if (var10 <= (double)(par9 * par9))
-	        {
-	            FontRenderer var12 = this.getFontRendererFromRenderManager();
-	            float var13 = 1.6F;
-	            float var14 = 0.016666668F * var13;
-	            GL11.glPushMatrix();
-	            GL11.glTranslatef((float)par3 + 0.0F, (float)par5 +1.1f+ entityLiving.height * ((EntityPixelmon) entityLiving).getScale(), (float)par7);
-	            GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-	            GL11.glRotatef(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-	            GL11.glRotatef(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
-	            GL11.glScalef(-var14, -var14, var14);
-	            GL11.glDisable(GL11.GL_LIGHTING);
-	            GL11.glDepthMask(false);
-	            GL11.glDisable(GL11.GL_DEPTH_TEST);
-	            GL11.glEnable(GL11.GL_BLEND);
-	            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-	            Tessellator var15 = Tessellator.instance;
-	            byte var16 = 0;
+		if (var10 <= (double) (par9 * par9)) {
+			FontRenderer var12 = this.getFontRendererFromRenderManager();
+			float var13 = 1.6F;
+			float var14 = 0.016666668F * var13;
+			GL11.glPushMatrix();
+			GL11.glTranslatef((float) par3 + 0.0F, (float) par5 + 1.1f + entityLiving.height * ((EntityPixelmon) entityLiving).getScale(), (float) par7);
+			GL11.glNormal3f(0.0F, 1.0F, 0.0F);
+			GL11.glRotatef(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+			GL11.glRotatef(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+			GL11.glScalef(-var14, -var14, var14);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			GL11.glDepthMask(false);
+			GL11.glDisable(GL11.GL_DEPTH_TEST);
+			GL11.glEnable(GL11.GL_BLEND);
+			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+			Tessellator var15 = Tessellator.instance;
+			byte var16 = 0;
 
-	            GL11.glDisable(GL11.GL_TEXTURE_2D);
-	            var15.startDrawingQuads();
-	            int var17 = var12.getStringWidth(par2Str) / 2;
-	            var15.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
-	            var15.addVertex((double)(-var17 - 1), (double)(-1 + var16), 0.0D);
-	            var15.addVertex((double)(-var17 - 1), (double)(8 + var16), 0.0D);
-	            var15.addVertex((double)(var17 + 1), (double)(8 + var16), 0.0D);
-	            var15.addVertex((double)(var17 + 1), (double)(-1 + var16), 0.0D);
-	            var15.draw();
-	            GL11.glEnable(GL11.GL_TEXTURE_2D);
-	            var12.drawString(par2Str, -var12.getStringWidth(par2Str) / 2, var16, 553648127);
-	            GL11.glEnable(GL11.GL_DEPTH_TEST);
-	            GL11.glDepthMask(true);
-	            var12.drawString(par2Str, -var12.getStringWidth(par2Str) / 2, var16, -1);
-	            GL11.glEnable(GL11.GL_LIGHTING);
-	            GL11.glDisable(GL11.GL_BLEND);
-	            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-	            GL11.glPopMatrix();
-	        }
-	    }
-	
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			var15.startDrawingQuads();
+			int var17 = var12.getStringWidth(par2Str) / 2;
+			var15.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
+			var15.addVertex((double) (-var17 - 1), (double) (-1 + var16), 0.0D);
+			var15.addVertex((double) (-var17 - 1), (double) (8 + var16), 0.0D);
+			var15.addVertex((double) (var17 + 1), (double) (8 + var16), 0.0D);
+			var15.addVertex((double) (var17 + 1), (double) (-1 + var16), 0.0D);
+			var15.draw();
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+			var12.drawString(par2Str, -var12.getStringWidth(par2Str) / 2, var16, 553648127);
+			GL11.glEnable(GL11.GL_DEPTH_TEST);
+			GL11.glDepthMask(true);
+			var12.drawString(par2Str, -var12.getStringWidth(par2Str) / 2, var16, -1);
+			GL11.glEnable(GL11.GL_LIGHTING);
+			GL11.glDisable(GL11.GL_BLEND);
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			GL11.glPopMatrix();
+		}
+	}
+
 	public void drawExpBar(EntityLiving entityLiving, double d, double d1, double d2, float f, float f1) {
 		float f2 = 1.6F;
 		float f3 = 0.01666667F * f2;
