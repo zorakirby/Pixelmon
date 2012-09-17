632f43e5ade01ac0c2c7826ddb1bc94182198545
Matthew <Matthew@Matthew-PC>
2012-09-17 16:41:43 -0400

    Kabuto Animated

--- minecraft/pixelmon/models/pokemon/ModelKabuto.java
+++ minecraft/pixelmon/models/pokemon/ModelKabuto.java
@@ -176,6 +176,14 @@ public class ModelKabuto extends ModelBase
   public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
   {
     super.setRotationAngles(f, f1, f2, f3, f4, f5);
+    LegFL.rotateAngleY = MathHelper.cos(f* 2F + (float)Math.PI)* f1 * .8F;
+    LegBL.rotateAngleY = MathHelper.cos(f* 2F)* f1 * .8F;
+    LegFR.rotateAngleY = MathHelper.cos(f* 2F)* f1 * .8F;
+    LegBR.rotateAngleY = MathHelper.cos(f* 2F + (float)Math.PI)* f1 * .8F;
+    LegFL.rotateAngleZ = MathHelper.sin(f* 2F + (float)Math.PI)* f1 * .5F;
+    LegBL.rotateAngleZ = MathHelper.sin(f* 2F)* f1 * .5F;
+    LegFR.rotateAngleZ = MathHelper.sin(f* 2F)* f1 * .5F;
+    LegBR.rotateAngleZ = MathHelper.sin(f* 2F + (float)Math.PI)* f1 * .5F;
   }
 
 }
