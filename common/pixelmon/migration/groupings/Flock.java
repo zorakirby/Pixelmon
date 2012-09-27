88eec37fafa4dc4089dc7685e43823c4254fe1dc
Michael Barre <mcbarre12@gmail.com>
2012-09-26 23:14:51 -0500

    removed some comments

--- common/pixelmon/migration/groupings/Flock.java
+++ common/pixelmon/migration/groupings/Flock.java
@@ -3,7 +3,7 @@ package pixelmon.migration.groupings;
 public class Flock {
 
 	public Flock() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
--- common/pixelmon/migration/groupings/Herd.java
+++ common/pixelmon/migration/groupings/Herd.java
@@ -3,7 +3,7 @@ package pixelmon.migration.groupings;
 public class Herd {
 
 	public Herd() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
