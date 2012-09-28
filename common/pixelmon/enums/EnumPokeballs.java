84ae25f114e63576e6a51e39bd30fffedc4543cc
Matthew-PC <godofwar00052694@yahoo.com>
2012-09-27 20:34:33 -0400

    More Pokeball index's added in.

--- common/pixelmon/enums/EnumPokeballs.java
+++ common/pixelmon/enums/EnumPokeballs.java
@@ -5,7 +5,7 @@ import pixelmon.config.PixelmonItemsPokeballs;
 import net.minecraft.src.Item;
 
 public enum EnumPokeballs {
-	PokeBall(0, 1, "pokeball", 0, 0, 0, 0, 0, 0), GreatBall(1, 1.5, "greatball", 0, 1, 0, 0, 0, 0), UltraBall(2, 2, "ultraball", 0, 2, 0, 0, 0, 0), MasterBall(3, 255, "masterball", 0, 3, 0, 0, 0, 0), LevelBall(
+	PokeBall(0, 1, "pokeball", 0, 0, 5, 13, 5, 14), GreatBall(1, 1.5, "greatball", 0, 1, 6, 13, 6, 14), UltraBall(2, 2, "ultraball", 0, 2, 7, 13, 7, 14), MasterBall(3, 255, "masterball", 0, 3, 0, 0, 0, 0), LevelBall(
 			4, 1, "levelball", 1, 8, 16, 13, 16, 14), MoonBall(5, 1, "moonball", 1, 15, 15, 13, 15, 14);
 
 	private EnumPokeballs(int index, double ballBonus, String filenamePrefix, int iconIndexX, int iconIndexY, int lidIconIndexX, int lidIconIndexY,
