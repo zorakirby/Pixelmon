8832285e341963a99d3f890a5af41319933e8a59
MrMasochism <malc.geddes@gmail.com>
2012-10-03 20:50:45 -0700

    added a few more stub classes for migration

---
+++ common/pixelmon/migration/worldRenderer/TownPlotter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pixelmon.migration.worldRenderer;
+
+/**
+ * @author Malcolm
+ *
+ *	Plots the location of the world's town and cities
+ */
+public class TownPlotter {
+
+	/**
+	 * 
+	 */
+	public TownPlotter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ common/pixelmon/migration/worldRenderer/WorldRenderer.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pixelmon.migration.worldRenderer;
+
+/**
+ * @author Malcolm
+ * 
+ *         This class renders the world as given by the worldProvider. Will turn
+ *         the world into a biome map that the migration plotters can work with
+ */
+public class WorldRenderer {
+
+	/**
+	 * 
+	 */
+	public WorldRenderer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
