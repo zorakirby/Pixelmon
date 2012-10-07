c94c33812b74f19af7cdc78f030d5d9a67fb7f1e
MrMasochism <malc.geddes@gmail.com>
2012-10-06 19:06:57 -0700

    fixed sprite lighting

--- minecraft/pixelmon/gui/inventoryExtended/GuiInventoryPixelmonExtended.java
+++ minecraft/pixelmon/gui/inventoryExtended/GuiInventoryPixelmonExtended.java
@@ -54,24 +54,23 @@ public class GuiInventoryPixelmonExtended extends GuiInventory {
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
 		int var4 = this.mc.renderEngine.getTexture("/gui/inventory.png");
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.renderEngine.bindTexture(var4);
-        this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.mc.renderEngine.bindTexture(var4);
+		this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
 		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, this.mc.renderEngine.getTexture("/pixelmon/gui/pixelmonOverlayExtended2.png"));
-		this.drawTexturedModalRect(width/2 -130, height/2-83, 0, 0, 160, 182);
-        func_74223_a(this.mc, guiLeft + 51, guiTop + 75, 30, (float)(guiLeft + 51) - 102, (float)(guiTop + 75 - 50) - 100);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, this.mc.renderEngine.getTexture("/pixelmon/gui/pixelmonOverlayExtended2.png"));
+		this.drawTexturedModalRect(width / 2 - 130, height / 2 - 83, 0, 0, 160, 182);
+
 		ScaledResolution var5 = new ScaledResolution(Minecraft.getMinecraft().gameSettings, Minecraft.getMinecraft().displayWidth,
 				Minecraft.getMinecraft().displayHeight);
 		int var6 = var5.getScaledWidth();
 		int var7 = var5.getScaledHeight();
 		int textureIndex;
 		GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_FOG);
-        Tessellator var2 = Tessellator.instance;
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		GL11.glDisable(GL11.GL_FOG);
+		Tessellator var2 = Tessellator.instance;
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		Minecraft.getMinecraft().entityRenderer.setupOverlayRendering();
-		RenderHelper.enableGUIStandardItemLighting();
 
 		fontRenderer.setUnicodeFlag(true);
 		int i = 0;
@@ -89,6 +88,7 @@ public class GuiInventoryPixelmonExtended extends GuiInventory {
 
 				i = p.order;
 				fontRenderer.drawString(displayName, 32, var7 / 6 + i * 30 + 6, 0xFFFFFF);
+
 				GL11.glBindTexture(GL11.GL_TEXTURE_2D, this.mc.renderEngine.getTexture("/pixelmon/gui/pixelmonOverlayExtended2.png"));
 				if (p.isMale)
 					this.drawTexturedModalRect(fontRenderer.getStringWidth(displayName) + 35, var7 / 6 + i * 30 + 6 + offset, 33, 208, 5, 9);
@@ -103,17 +103,18 @@ public class GuiInventoryPixelmonExtended extends GuiInventory {
 				else
 					numString = "" + p.nationalPokedexNumber;
 				int spriteIndex;
-				
+
 				if (p.isShiny)
 					spriteIndex = Minecraft.getMinecraft().renderEngine.getTexture("/pixelmon/shinysprites/" + numString + ".png");
 				else
 					spriteIndex = Minecraft.getMinecraft().renderEngine.getTexture("/pixelmon/sprites/" + numString + ".png");
 				drawImageQuad(spriteIndex, 3, var7 / 6 + i * 30 + 3 + offset, 24f, 24f, 0f, 0f, 1f, 1f);
+
 				if (p.heldItem != null) {
 					spriteIndex = Minecraft.getMinecraft().renderEngine.getTexture("/pixelmon/image/pitems.png");
 					drawImageQuad(spriteIndex, 17, var7 / 6 + i * 30 + 17 + offset, 8, 8, 0f, 0f, 16f / 256f, 16f / 256f);
 				}
-
+				
 				fontRenderer.drawString("Lvl " + p.lvl, 32, var7 / 6 + i * 30 + fontRenderer.FONT_HEIGHT + 7 + offset, 0xFFFFFF);
 				if (p.health <= 0) {
 					fontRenderer.drawString("Fainted", 33 + fontRenderer.getStringWidth("Lvl " + p.lvl), var7 / 6 + i * 30 + fontRenderer.FONT_HEIGHT + 7
@@ -134,6 +135,8 @@ public class GuiInventoryPixelmonExtended extends GuiInventory {
 		GL11.glDisable(GL11.GL_LIGHTING);
 		GL11.glDepthMask(true);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		func_74223_a(this.mc, guiLeft + 51, guiTop + 75, 30, (float) (guiLeft + 51) - 102, (float) (guiTop + 75 - 50) - 100);
+
 	}
 
 	private void drawImageQuad(int textureHandle, int x, int y, float w, float h, float us, float vs, float ue, float ve) {
