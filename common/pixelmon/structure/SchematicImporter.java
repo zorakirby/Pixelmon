12f716aff81ee59ae2fbda23d29c673e52c78c55
MrMasochism <malc.geddes@gmail.com>
2012-10-10 20:28:27 -0700

    schematic importer now loads grid of block ids

--- common/pixelmon/structure/SchematicImporter.java
+++ common/pixelmon/structure/SchematicImporter.java
@@ -20,6 +20,7 @@ import net.minecraft.src.NBTTagCompound;
 
 public class SchematicImporter {
 	private String filename;
+	private int [][][] blocks;
 
 	public SchematicImporter(String filename) {
 		this.filename = filename;
@@ -29,16 +30,22 @@ public class SchematicImporter {
 		NBTTagCompound n = getNBTTag();
 		if (n == null)
 			return;
-		Iterator iterator = n.getTags().iterator();
-		do {
-			if (!iterator.hasNext())
-				break;
-
-			NBTBase nbtbase = (NBTBase) iterator.next();
-
-			if (nbtbase instanceof NBTTagCompound) {
+		
+		int width = n.getShort("Width");
+		int height = n.getShort("Height");
+		int length = n.getShort("Length");
+		
+		blocks = new int[width][height][length];
+		byte[] blockArray = n.getByteArray("Blocks");
+		int i=0;
+		for (int y =0; y < length; y++){
+			for (int z = 0; z < height; z++){
+				for (int x =0; x <width; x++){
+					blocks[x][z][y] = blockArray[i++];
+				}
 			}
-		} while (true);
+		}
+
 	}
 
 	NBTTagCompound getNBTTag() {
