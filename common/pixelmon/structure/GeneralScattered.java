08564a8c1f883d29e2595ab0ca385014ce1cc85b
MrMasochism <malc.geddes@gmail.com>
2012-10-18 08:18:14 -0700

    added block generation to generalScattered

--- common/pixelmon/structure/GeneralScattered.java
+++ common/pixelmon/structure/GeneralScattered.java
@@ -13,11 +13,17 @@ public class GeneralScattered extends StructureScattered{
 	
 	protected GeneralScattered(Random par1Random, int par2, int par3, SchematicImporter schematic) {
 		super(par1Random, par2, 64, par3, schematic.width, schematic.height, schematic.length);
+		s = schematic;
 	}
 
-    public boolean addComponentParts(World par1World, Random par2Random, StructureBoundingBox par3StructureBoundingBox)
+    public boolean addComponentParts(World world, Random par2Random, StructureBoundingBox bb)
     {
-		return false;
+    	for (int x =0; x < s.width; x++)
+    		for (int z = 0; z< s.length; z++)
+    			for (int y = 0; y < s.height; y++)
+    				this.placeBlockAtCurrentPosition(world, 0, 0, x, y, z, bb);
+    				// 									id, metadata
+		return true;
     }
 	
 }
