c54da2ed523d3563d26d7abda82ef0fce62c05e0
MrMasochism <malc.geddes@gmail.com>
2012-10-20 09:37:32 -0700

    reverted to repo code

--- common/pixelmon/structure/SchematicImporter.java
+++ common/pixelmon/structure/SchematicImporter.java
@@ -59,9 +59,9 @@ public class SchematicImporter {
 		for (int y = 0; y < length; y++) {
 			for (int z = 0; z < height; z++) {
 				for (int x = 0; x < width; x++) {
-					blocks[x][z][y] = blockArray[i];
+					blocks[x][z][y] = blockArray[i++];
 					blockData[x][z][y] = blockDataArray[i];
-					tileEntities[x][z][y] = tileEntitiesArray[i++];
+					tileEntities[x][z][y] = tileEntitiesArray[i];
 				}
 			}
 		}
