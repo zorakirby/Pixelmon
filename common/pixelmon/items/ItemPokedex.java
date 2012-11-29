16144bd55009a6265cdb7b157b37244231af4cda
MrMasochism <malc.geddes@gmail.com>
2012-11-30 07:40:55 +1300

    fix to pokedex crash

--- common/pixelmon/items/ItemPokedex.java
+++ common/pixelmon/items/ItemPokedex.java
@@ -6,30 +6,26 @@ import pixelmon.storage.PixelmonStorage;
 import pixelmon.storage.PokeballManager;
 import net.minecraft.src.*;
 
-public class ItemPokedex extends Item
-{
+public class ItemPokedex extends Item {
 
-	public ItemPokedex(int par1) 
-	{
+	public ItemPokedex(int par1) {
 		super(par1);
 		setIconIndex(22);
 		setTextureFile("/pixelmon/image/pitems.png");
 		setCreativeTab(CreativeTabs.tabTools);
 	}
-	
-	public ItemStack onItemRightClick(ItemStack i, World world, EntityPlayer player)
-	{
-		//openPokedexGui(1, player, world);
+
+	public ItemStack onItemRightClick(ItemStack i, World world, EntityPlayer player) {
+		if (!world.isRemote)
+			openPokedexGui(1, player, world);
 		return i;
 	}
-	
-	public EnumRarity getRarity(ItemStack i)
-	{
+
+	public EnumRarity getRarity(ItemStack i) {
 		return EnumRarity.rare;
 	}
-	
-	public void openPokedexGui(int i, EntityPlayer player, World world)
-	{
+
+	public void openPokedexGui(int i, EntityPlayer player, World world) {
 		PokeballManager pm = PixelmonStorage.PokeballManager;
 		EntityPlayerMP e = pm.getPlayerFromName(player.username);
 		pm.getPlayerStorage(e).pokedex.sendToPlayer(e);
