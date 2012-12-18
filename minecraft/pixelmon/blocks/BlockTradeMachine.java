dcdee9fee0b85ef3a0eb10d501053367a35b8146
Jaryt Bustard <Jaryt23@gmail.com>
2012-12-18 14:18:42 -0500

    Armor

--- minecraft/pixelmon/blocks/BlockTradeMachine.java
+++ minecraft/pixelmon/blocks/BlockTradeMachine.java
@@ -5,10 +5,13 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import pixelmon.comm.ChatHandler;
 
 public class BlockTradeMachine extends BlockContainer {
@@ -17,7 +20,6 @@ public class BlockTradeMachine extends BlockContainer {
 		super(id, Material.rock);
 		setHardness(3.5f);
 		setStepSound(Block.soundStoneFootstep);
-		setRequiresSelfNotify();
 	}
 
 	@Override
@@ -50,6 +52,11 @@ public class BlockTradeMachine extends BlockContainer {
 		this.setBlockBounds(1f, 0, 0f, 1f, 3f, 1f);
 	}
 
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLiving par5EntityLiving) {
+    	super.onBlockPlacedBy(par1World, par2, par3, par4, par5EntityLiving);
+    	setBlockBounds(par1World.getBlockMetadata(par2, par3, par4));
+    }
+	
 	/**
 	 * Called upon block activation (left or right click on the block.). The
 	 * three integers represent x,y,z of the block.
