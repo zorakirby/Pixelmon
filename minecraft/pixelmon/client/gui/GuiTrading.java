3ce56808d302f49d15b6da3fe5c4b29740e297e5
Jaryt Bustard <Jaryt23@gmail.com>
2012-12-22 14:16:43 -0500

    Trading Gui Fix

--- minecraft/pixelmon/client/gui/GuiTrading.java
+++ minecraft/pixelmon/client/gui/GuiTrading.java
@@ -281,7 +281,15 @@ public class GuiTrading extends GuiContainer {
 			drawTexturedModalRect((width + 65) / 2, (height + 85) / 2, 153, 242, 72, 14);
 		} else if (ClientTradingManager.player2Ready) {
 			GL11.glColor3f(0f, 1f, 0f);
-			drawTexturedModalRect((width + 65) / 2, (height + 85) / 2, 1, 242, 72, 14);
+			drawTexturedModalRect((width + 75) / 2, (height + 85) / 2, 1, 242, 58, 14);
+		}
+	
+		if (!ClientTradingManager.player1Ready) {
+		GL11.glColor3f(1f, 0f, 0f);
+		drawTexturedModalRect((width - 210) / 2, (height + 85) / 2, 153, 242, 72, 14);
+		} else if (ClientTradingManager.player1Ready) {
+			drawTexturedModalRect((width - 195) / 2, (height + 85) / 2, 1, 242, 58, 14);
+		GL11.glColor3f(0f, 1f, 0f);
 		}
 		GL11.glPopMatrix();
 
