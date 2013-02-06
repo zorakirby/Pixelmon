3c7005b52b5de8e0fbda56b87934adcf0e404863
MrMasochism <malc.geddes@gmail.com>
2013-02-06 16:13:03 +1300

    added headless skeletonBiped constructor

--- minecraft/pixelmon/client/models/animations/SkeletonBiped.java
+++ minecraft/pixelmon/client/models/animations/SkeletonBiped.java
@@ -8,11 +8,15 @@ public class SkeletonBiped extends SkeletonBase {
 	protected ModelRenderer leftArm, rightArm;
 	protected ModelRenderer leftLeg, rightLeg;
 	
-	public SkeletonBiped(ModelRenderer body, ModuleHead headModule, ModelRenderer leftArm, ModelRenderer rightArm, ModelRenderer leftLeg, ModelRenderer rightLeg) {
+	public SkeletonBiped(ModelRenderer body, ModelRenderer leftArm, ModelRenderer rightArm, ModelRenderer leftLeg, ModelRenderer rightLeg){
 		super(body);
-		modules.add(headModule);
 		this.leftArm = leftArm; this.rightArm = rightArm;
 		this.leftLeg = leftLeg; this.rightLeg = rightLeg;
+	}
+	
+	public SkeletonBiped(ModelRenderer body, ModuleHead headModule, ModelRenderer leftArm, ModelRenderer rightArm, ModelRenderer leftLeg, ModelRenderer rightLeg) {
+		this(body, leftArm, rightArm, leftLeg, rightLeg);
+		modules.add(headModule);
 	}
 
 	@Override
