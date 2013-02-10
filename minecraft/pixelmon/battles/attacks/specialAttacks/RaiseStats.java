4f3ccf4cd3ae257353fcf2eaca88eee5aa40126c
Hiroku <Paul_L_Preston@hotmail.com>
2013-02-10 14:44:43 +1000

    Finished RaiseStats. In case your wondering, thats for ominous wind, acient power, etc.

--- minecraft/pixelmon/battles/attacks/specialAttacks/RaiseStats.java
+++ minecraft/pixelmon/battles/attacks/specialAttacks/RaiseStats.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import pixelmon.battles.attacks.Attack;
+import pixelmon.comm.ChatHandler;
 import pixelmon.entities.pixelmon.EntityPixelmon;
 
 public class RaiseStats extends SpecialAttackBase {
@@ -18,8 +19,14 @@ public class RaiseStats extends SpecialAttackBase {
 			Attack a, ArrayList<String> attackList,
 			ArrayList<String> targetAttackList) throws Exception {
 		Random rand = new Random();
-		if(rand.nextInt(10)+1 == 1){
-			
+		if(rand.nextInt(10)+1 == 1)
+		{
+			user.battleStats.IncreaseAttack(1);
+			user.battleStats.IncreaseDefence(1);
+			user.battleStats.IncreaseSpecialAttack(1);
+			user.battleStats.IncreaseSpecialDefence(1);
+			user.battleStats.IncreaseSpeed(1);
+			ChatHandler.sendBattleMessage(user.getOwner(), target.getOwner(), user.getNickname() + "'s stats rose!");
 		}
 		
 		return false;
