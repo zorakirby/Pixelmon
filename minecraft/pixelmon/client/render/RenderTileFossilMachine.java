791d88dc910781753f7ccb609a7378fb37ddf682
MrMasochism <malc.geddes@gmail.com>
2013-02-10 17:13:20 +1300

    Fixed blue bar being rendered through terrain

--- minecraft/pixelmon/client/render/RenderTileFossilMachine.java
+++ minecraft/pixelmon/client/render/RenderTileFossilMachine.java
@@ -188,7 +188,6 @@ public class RenderTileFossilMachine extends TileEntitySpecialRenderer {
 		GL11.glScalef(-f3, -f3, f3);
 		GL11.glDisable(2896 /* GL_LIGHTING */);
 		GL11.glDepthMask(false);
-		GL11.glDisable(2929 /* GL_DEPTH_TEST */);
 		GL11.glEnable(3042 /* GL_BLEND */);
 		GL11.glBlendFunc(770, 771);
 		Tessellator tessellator = Tessellator.instance;
@@ -208,7 +207,6 @@ public class RenderTileFossilMachine extends TileEntitySpecialRenderer {
 		tessellator.addVertex(f8 - 25F, -7 + byte0, 0.0D);
 		tessellator.draw();
 		GL11.glEnable(3553 /* GL_TEXTURE_2D */);
-		GL11.glEnable(2929 /* GL_DEPTH_TEST */);
 		GL11.glDepthMask(true);
 		GL11.glEnable(2896 /* GL_LIGHTING */);
 		GL11.glDisable(3042 /* GL_BLEND */);
