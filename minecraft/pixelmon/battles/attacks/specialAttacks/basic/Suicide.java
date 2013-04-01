aeb258e166ceb680eb8d25ea994fb3e8100fa58a
Hiroku <Paul_L_Preston@hotmail.com>
2013-04-01 20:42:50 +1000

    Fixed Self-Destruct and Explosion (tested both)

--- minecraft/pixelmon/battles/attacks/specialAttacks/basic/Suicide.java
+++ minecraft/pixelmon/battles/attacks/specialAttacks/basic/Suicide.java
@@ -3,6 +3,7 @@ package pixelmon.battles.attacks.specialAttacks.basic;
 import java.util.ArrayList;
 
 import pixelmon.battles.attacks.Attack;
+import pixelmon.comm.ChatHandler;
 import pixelmon.entities.pixelmon.EntityPixelmon;
 
 public class Suicide extends SpecialAttackBase {
@@ -18,6 +19,7 @@ public class Suicide extends SpecialAttackBase {
 			ArrayList<String> targetAttackList) throws Exception {
 	
 		user.setEntityHealth(0);
+		ChatHandler.sendBattleMessage(user.getOwner(),target.getOwner(), user.getNickname() + " blew itself up!");
 		
 		return false;
 	}
