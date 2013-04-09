cd22d2464079fd14f262946d6496350407244062
Hiroku <Paul_L_Preston@hotmail.com>
2013-04-09 22:00:01 +1000

    Wrote generalized class for OHKO Moves. Accuracy methods weren't working too.

--- minecraft/pixelmon/battles/attacks/specialAttacks/basic/OHKO.java
+++ minecraft/pixelmon/battles/attacks/specialAttacks/basic/OHKO.java
@@ -1,8 +1,12 @@
 package pixelmon.battles.attacks.specialAttacks.basic;
 
 import java.util.ArrayList;
+import java.util.Random;
 
+import net.minecraft.util.DamageSource;
 import pixelmon.battles.attacks.Attack;
+import pixelmon.battles.attacks.Value;
+import pixelmon.comm.ChatHandler;
 import pixelmon.entities.pixelmon.EntityPixelmon;
 
 public class OHKO extends SpecialAttackBase {
@@ -10,15 +14,32 @@ public class OHKO extends SpecialAttackBase {
 	public OHKO() {
 		super(ApplyStage.During, false);
 	}
-
+	
 	@Override
-	public boolean ApplyEffect(EntityPixelmon user, EntityPixelmon target,
-			Attack a, double crit, ArrayList<String> attackList,
-			ArrayList<String> targetAttackList) throws Exception {
+	public boolean ApplyEffect(EntityPixelmon user, EntityPixelmon target, Attack a, double crit, ArrayList<String> attackList, ArrayList<String> targetAttackList) throws Exception {
 		
-			a.baseAttack.accuracy = (user.getLvl().getLevel() - target.getLvl().getLevel()) + 30;
+		int chance;
+		if (target.getLvl().getLevel() > user.getLvl().getLevel())
+			chance = 0;
 			
-			return false;
+		else 
+			chance = user.getLvl().getLevel() - target.getLvl().getLevel() + 30;
+			
+		Random rand = new Random();
+		int x = rand.nextInt(100)+1;
+		
+		//System.out.println(x);
+		//System.out.println(chance);
+		
+		if(x < chance){
+		target.attackEntityFrom(DamageSource.causeMobDamage(user), target.getHealth());
+		ChatHandler.sendBattleMessage(user.getOwner(), target.getOwner(), "It's a one-hit-KO!");
+		}
+		
+		else if (x > chance)
+			ChatHandler.sendBattleMessage(user.getOwner(), target.getOwner(), "The move failed!");
+		
+		return false;
 	}
 
 }
