8e3844a7c12f26ba5d735a1d5eb9623d767c482f
MrMasochism <malc.geddes@gmail.com>
2013-05-12 16:25:06 +1200

    Re-implemented proper fire stone spawning.  Added exception for spawn conditions for jungle spawns

--- minecraft/pixelmon/client/gui/battles/GuiBattle.java
+++ minecraft/pixelmon/client/gui/battles/GuiBattle.java
@@ -128,6 +128,7 @@ public class GuiBattle extends GuiContainer {
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		drawImageQuad(width - 120, height - (guiHeight + 45), 120, 45, 0, 0, 120f / 128f, 45f / 64f);
 		PixelmonDataPacket userPokemon = ClientBattleManager.getUserPokemonPacket();
+		if (userPokemon==null) return;
 		String name = userPokemon.nickname.equals("") ? userPokemon.name : userPokemon.nickname;
 		drawString(fontRenderer, name, width - 113, height - (guiHeight + 37), 0xFFFFFF);
 		mc.renderEngine.bindTexture("/pixelmon/gui/pokemonInfoP1.png");
