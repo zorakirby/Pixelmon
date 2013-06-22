710f3a17cdceb17ccbf0d760b1f4d06939ada312
Hiroku <Paul_L_Preston@hotmail.com>
2013-06-22 21:48:32 +1000

    Rollout stuff

--- minecraft/pixelmon/battles/attacks/specialAttacks/multiTurn/Rollout.java
+++ minecraft/pixelmon/battles/attacks/specialAttacks/multiTurn/Rollout.java
@@ -4,50 +4,55 @@ import java.util.ArrayList;
 
 import pixelmon.RandomHelper;
 import pixelmon.battles.attacks.Attack;
-import pixelmon.database.DatabaseMoves;
 import pixelmon.entities.pixelmon.EntityPixelmon;
 
 public class Rollout extends MultiTurnSpecialAttackBase {
 	boolean hitsTarget;
-	int timesHit = 0;
 	@Override
 	public boolean ApplyEffect(EntityPixelmon user, EntityPixelmon target,
 			Attack a, double crit, ArrayList<String> attackList,
 			ArrayList<String> targetAttackList) throws Exception {
 			System.out.println("Runs class");
-			if(getTurnCount(user) == 5)
+			if(!doesPersist(user))
+			{								// Applies the beginning of the attack
+					setTurnCount(user, 5);
+					setPersists(user, true);
+					setHitCount(user);
+			}
+			if(getTurnCount(user) == 0)
 			{
+				System.out.println("Turncount is 0");
 				setPersists(user, false);
 				return true;
 			}
-			if(!doesPersist(user))
-			{
-					setTurnCount(user, 5);
-					setPersists(user, true);
-			}
 			else
 			{
-				if(timesHit == getTurnCount(user) && 
-						(a.baseAttack.accuracy/100)* (user.battleStats.getAccuracy()/target.battleStats.getEvasion())
-						<= RandomHelper.getRandomNumberBetween(1, 100))
+				int randomNumber = RandomHelper.getRandomNumberBetween(1, 100);
+				double ratioShit = a.baseAttack.accuracy * (user.battleStats.getAccuracy()/target.battleStats.getEvasion());
+				if((getHitCount(user) == 5 - getTurnCount(user)) && 
+						ratioShit
+						>= randomNumber)
 				{
-					a.baseAttack.basePower = (int) (30 * Math.pow(2, getTurnCount(user)));
-					if(attackList.contains(DatabaseMoves.getAttack("Defence Curl")))
-					timesHit++;
-					System.out.println(a.baseAttack.basePower);
+					a.baseAttack.basePower = (int) (30 * Math.pow(2, 5 - getTurnCount(user)));
+					//if(attackList.contains(DatabaseMoves.getAttack("Defense Curl")))
+						
+					
 					hitsTarget = true;
+					System.out.println((double)(a.baseAttack.accuracy/100)*(user.battleStats.getAccuracy()/target.battleStats.getEvasion()));
+					System.out.println(randomNumber);
+					System.out.println(ratioShit);
+					decrementTurnCount(user);
+					incrementHitCount(user);
+					return false;
 				}
 				else
 				{
+					System.out.println(getHitCount(user) + " " + getTurnCount(user) + " " + a.baseAttack.accuracy);
 					setPersists(user, false);
 					return true;
 				}
 					
 			}
-			
-		
-		
-		return false;
 	}
 
 	@Override
