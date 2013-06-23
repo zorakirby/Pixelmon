abca836b6f7b74427e543f300c1796d575630499
MrMasochism <malc.geddes@gmail.com>
2013-06-24 07:19:39 +1200

    allowed ability to remove rare candy crafting

--- minecraft/pixelmon/structure/BlockSwitch.java
+++ minecraft/pixelmon/structure/BlockSwitch.java
@@ -1,5 +1,16 @@
 package pixelmon.structure;
 
+import pixelmon.config.PixelmonBlocks;
+import net.minecraft.block.Block;
+
 public class BlockSwitch {
+	public int initBlockID;
+	public Block replacementBlock;
 	
+	public BlockSwitch(int initBlockID, String replacementString){
+		this.initBlockID = initBlockID;
+		if (replacementString.equalsIgnoreCase("healer")){
+			replacementBlock = PixelmonBlocks.healer;
+		}
+	}
 }
