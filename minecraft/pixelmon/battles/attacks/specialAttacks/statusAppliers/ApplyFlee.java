fe67765aff17faf25d11bbe6cacfc6b9e97e8cd9
KaiBB <mc.kaibb@gmail.com>
2013-07-07 17:24:53 -0400

    The Flee effect no longer works during Trainer battles.

--- minecraft/pixelmon/battles/attacks/specialAttacks/statusAppliers/ApplyFlee.java
+++ minecraft/pixelmon/battles/attacks/specialAttacks/statusAppliers/ApplyFlee.java
@@ -3,6 +3,7 @@ package pixelmon.battles.attacks.specialAttacks.statusAppliers;
 import java.util.ArrayList;
 
 import pixelmon.battles.attacks.Attack;
+import pixelmon.comm.ChatHandler;
 import pixelmon.entities.pixelmon.Entity7HasAI.Aggression;
 import pixelmon.entities.pixelmon.EntityPixelmon;
 
@@ -10,9 +11,11 @@ public class ApplyFlee extends StatusApplierBase {
 
 	@Override
 	public void ApplyEffect(Attack attack, double crit, EntityPixelmon user, EntityPixelmon target, ArrayList<String> attackList, ArrayList<String> targetAttackList) {
-		target.aggression = Aggression.passive;
-		user.aggression = Aggression.passive;
-
-		target.battleController.endBattleWithoutXP();
+		
+		if(!target.hasOwner() || !user.hasOwner()){
+			target.aggression = Aggression.passive;
+			user.aggression = Aggression.passive;
+			target.battleController.endBattleWithoutXP();
+		}
 	}
 }
