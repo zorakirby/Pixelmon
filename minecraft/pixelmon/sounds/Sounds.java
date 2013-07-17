15bf0b54e159660d24d70fd75adaf4ca2e1011bb
MrMasochism <malc.geddes@gmail.com>
2013-07-17 23:09:53 +1200

    updated stuff a bit

--- minecraft/pixelmon/sounds/Sounds.java
+++ minecraft/pixelmon/sounds/Sounds.java
@@ -8,6 +8,8 @@ import net.minecraft.client.audio.SoundPool;
 import net.minecraft.client.resources.ResourceManager;
 import net.minecraft.util.ResourceLocation;
 import pixelmon.enums.EnumPokemon;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.client.FMLFileResourcePack;
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -27,7 +29,7 @@ public class Sounds {
 	@SideOnly(Side.CLIENT)
 	public static boolean installMusic() {
 		System.out.println("Checking for pixelmon resources");
-		ResourceLocation resourceLocation = new ResourceLocation("pixelmon:music");
+		FMLFileResourcePack resourcePack = (FMLFileResourcePack)FMLClientHandler.instance().getResourcePackFor("pixelmon");
 		ResourceManager r = Minecraft.getMinecraft().func_110442_L();
 		File musicDir = new File(Minecraft.getMinecraft().mcDataDir + "/resources/music/pixelmon");
 		File[] musicFiles = musicDir.listFiles();
