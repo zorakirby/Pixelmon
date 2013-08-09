901c4ae1239686fbe90dab97525ab0ae1b1d9e01
Rhonim <chinchilla3210@yahoo.com>
2013-08-09 02:21:37 -0400

    Create EntityDeath.java

---
+++ minecraft/pixelmon/entities/EntityDeath.java
@@ -0,0 +1,18 @@
+package pixelmon.entities;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+
+import pixelmon.comm.EnumPackets;
+import pixelmon.comm.PacketCreator;
+
+public class EntityDeath {
+  @ForgeSubscribe
+	public void onPlayerDeath(LivingDeathEvent event) {
+		if(event.entity instanceof EntityPlayerMP) {
+			((EntityPlayerMP) event.entity).playerNetServerHandler.sendPacketToPlayer(PacketCreator.createPacket(EnumPackets.PlayerDeath));
+		}
+	}
+}
