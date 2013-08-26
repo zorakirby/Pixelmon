2b231e23762604c1a91881d24044a252ee65f508
Xwaffle1 <bucket123456745@yahoo.com>
2013-08-25 22:01:06 -0400

    Generation Recognition, rarity check for an error.

--- minecraft/pixelmon/spawning/SpawnRegistry.java
+++ minecraft/pixelmon/spawning/SpawnRegistry.java
@@ -9,12 +9,14 @@ import pixelmon.config.PixelmonEntityList.ClassType;
 import pixelmon.database.DatabaseStats;
 import pixelmon.database.DatabaseTrainers;
 import pixelmon.database.SpawnLocation;
+import pixelmon.entities.pixelmon.stats.BaseStats;
 
 public class SpawnRegistry {
 
 	private static HashMap<BiomeGenBase, List<SpawnData>> biomeSpawns = new HashMap<BiomeGenBase, List<SpawnData>>();
 	private static HashMap<BiomeGenBase, List<SpawnData>> undergroundSpawns = new HashMap<BiomeGenBase, List<SpawnData>>();
 	private static HashMap<BiomeGenBase, List<SpawnData>> biomeWaterSpawns = new HashMap<BiomeGenBase, List<SpawnData>>();
+	private static DatabaseStats db;
 
 	public static void addSpawn(String name, int rarity, ClassType type) {
 		BiomeGenBase[] biomes = null;
@@ -58,6 +60,34 @@ public class SpawnRegistry {
 		spawnList.add(new SpawnData(name, rarity, type, s));
 		hashmap.put(b, spawnList);
 	}
+
+	public static void getGenerationInfo(HashMap<String, String> hashmap, String name) {
+		List<SpawnData> spawnList = new ArrayList<SpawnData>();
+		BaseStats stats = db.GetBaseStats(name);
+		int ID = stats.nationalPokedexNumber;
+		// for (SpawnData pixelmon : spawnList) {
+		if (!hashmap.containsKey(name)) {
+			if (ID <= 151) {
+			//	System.out.println("Generation 1: " + name);
+				hashmap.put(name, "Gen1");
+			} else if (ID > 151 && ID <= 251) {
+				//System.out.println("Generation 2: " + name);
+				hashmap.put(name, "Gen2");
+			} else if (ID > 251 && ID <= 386) {
+			//	System.out.println("Generation 3: " + name);
+				hashmap.put(name, "Gen3");
+			} else if (ID > 386 && ID <= 493) {
+			//	System.out.println("Generation 4: " + name);
+				hashmap.put(name, "Gen4");
+			} else if (ID > 493 && ID <= 649) {
+			//	System.out.println("Generation 5: " + name);
+				hashmap.put(name, "Gen5");
+			}
+		}
+
+	}
+
+	// }
 
 	public static List<SpawnData> getSpawnsForBiome(BiomeGenBase b) {
 		return biomeSpawns.get(b);
