a9f4c3b9e88348adbc62bc942f4a630b0683eecb
MrMasochism <malc.geddes@gmail.com>
2013-09-08 20:49:16 +1200

    Fixed extended inv when held item equipped

--- minecraft/pixelmon/client/gui/inventoryExtended/GuiInventoryPixelmonExtended.java
+++ minecraft/pixelmon/client/gui/inventoryExtended/GuiInventoryPixelmonExtended.java
@@ -133,6 +133,7 @@ public class GuiInventoryPixelmonExtended extends GuiInventory {
 					mc.renderEngine.func_110577_a(GuiResources.shinySprite(numString));
 				else
 					mc.renderEngine.func_110577_a(GuiResources.sprite(numString));
+				GL11.glDisable(GL11.GL_LIGHTING);
 				drawImageQuad(slot.x, slot.y, 16f, 16f, 0f, 0f, 1f, 1f);
 
 				if (p.heldItemId != -1) {
