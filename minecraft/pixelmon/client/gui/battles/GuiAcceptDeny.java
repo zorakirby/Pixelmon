11a7860e9cd8cb36dbf9a7b05f065dba52455d92
MrMasochism <malc.geddes@gmail.com>
2013-09-12 07:45:41 +1200

    added player head render to accept deny gui

--- minecraft/pixelmon/client/gui/battles/GuiAcceptDeny.java
+++ minecraft/pixelmon/client/gui/battles/GuiAcceptDeny.java
@@ -1,6 +1,7 @@
 package pixelmon.client.gui.battles;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import cpw.mods.fml.common.network.PacketDispatcher;
 
@@ -12,7 +13,13 @@ import pixelmon.comm.EnumPackets;
 import pixelmon.comm.PacketCreator;
 import pixelmon.enums.EnumPokeballs;
 import pixelmon.gui.ContainerEmpty;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.inventory.Container;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -21,7 +28,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class GuiAcceptDeny extends GuiContainer {
 	public static BattleQueryPacket opponent;
-	static ResourceLocation main = new ResourceLocation("pixelmon:gui/acceptDeny/main.png");
+	static ResourceLocation main = new ResourceLocation("pixelmon:gui/acceptDeny/test.png");
 	static ResourceLocation button = new ResourceLocation("pixelmon:gui/acceptDeny/button.png");
 	static ResourceLocation buttonOver = new ResourceLocation("pixelmon:gui/acceptDeny/buttonOver.png");
 	boolean accepted = false;
@@ -76,24 +83,24 @@ public class GuiAcceptDeny extends GuiContainer {
 		}
 
 		GL11.glDisable(GL11.GL_LIGHTING);
-		fontRenderer.drawString(opponent.opponentName, (width - 280) / 2 + 183 - fontRenderer.getStringWidth(opponent.opponentName), (height - 182) / 2 + 107,
-				0xffffff);
+//		fontRenderer.drawString(opponent.opponentName, (width - 280) / 2 + 183 - fontRenderer.getStringWidth(opponent.opponentName), (height - 182) / 2 + 107,
+//				0xffffff);
 
 		pos = 0;
-		for (int pid : opponent.pokeballs) {
-			if (pid != -999) {
-				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-				if (pid < 0) {
-					pid *= -1;
-					pid--;
-					GL11.glColor4f(0.4F, 0.4F, 0.4F, 1.0F);
-				}
-				Item pball = EnumPokeballs.getFromIndex(pid).getItem();
-				itemRenderer.renderItemAndEffectIntoGUI(this.fontRenderer, this.mc.func_110434_K(), new ItemStack(pball), (width - 280) / 2 + 188 + pos * 12,
-						(height - 182) / 2 + 56);
-			}
-			pos++;
-		}
+//		for (int pid : opponent.pokeballs) {
+//			if (pid != -999) {
+//				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+//				if (pid < 0) {
+//					pid *= -1;
+//					pid--;
+//					GL11.glColor4f(0.4F, 0.4F, 0.4F, 1.0F);
+//				}
+//				Item pball = EnumPokeballs.getFromIndex(pid).getItem();
+//				itemRenderer.renderItemAndEffectIntoGUI(this.fontRenderer, this.mc.func_110434_K(), new ItemStack(pball), (width - 280) / 2 + 188 + pos * 12,
+//						(height - 182) / 2 + 56);
+//			}
+//			pos++;
+//		}
 		itemRenderer.renderWithColor = true;
 		GL11.glDisable(GL11.GL_LIGHTING);
 
@@ -116,8 +123,15 @@ public class GuiAcceptDeny extends GuiContainer {
 		fontRenderer.drawString("Accept", (width - 280) / 2 + 75 - fontRenderer.getStringWidth("Accept") / 2, (height - 182) / 2 + 158, 0xffffff);
 		fontRenderer.drawString("Decline", (width - 280) / 2 + 195 - fontRenderer.getStringWidth("Decline") / 2, (height - 182) / 2 + 158, 0xffffff);
 
+		drawEntity(Minecraft.getMinecraft().thePlayer, (width - 280) / 2 +19, (height - 182) / 2+11, 20, 0, 0);
 		GL11.glDisable(GL11.GL_BLEND);
 	}
+	
+	protected void drawEntity(EntityLivingBase entity, int par1, int par2, int par3, float par4, float par5) {
+		RenderManager.instance.renderEngine.func_110577_a(((AbstractClientPlayer) entity).func_110306_p());
+		GuiHelper.drawImageQuad(par1, par2, 60, 60, 0.11f, 0.26f, 0.26f, 0.49f, this.zLevel);
+	}
+
 
 	@Override
 	protected void mouseClicked(int mouseX, int mouseY, int par3) {
