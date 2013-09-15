fa3802d331738609cb00a2ab903c54b738c98bc5
Xwaffle1 <bucket123456745@yahoo.com>
2013-09-15 12:06:49 -0400

    Spawning is now configurable through generation. Rarity added to BaseStats

--- minecraft/pixelmon/spawning/SpawnRegistry.java
+++ minecraft/pixelmon/spawning/SpawnRegistry.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 
 import net.minecraft.world.biome.BiomeGenBase;
+import pixelmon.config.PixelmonConfig;
 import pixelmon.config.PixelmonEntityList.ClassType;
 import pixelmon.database.DatabaseStats;
 import pixelmon.database.DatabaseTrainers;
 import pixelmon.database.SpawnLocation;
 import pixelmon.entities.pixelmon.Entity3HasStats;
+import pixelmon.entities.pixelmon.EntityPixelmon;
 import pixelmon.entities.pixelmon.stats.BaseStats;
 
 public class SpawnRegistry {
@@ -41,8 +43,8 @@ public class SpawnRegistry {
 					if (type == ClassType.Pixelmon) {
 						if (s == SpawnLocation.Land || s == SpawnLocation.Air) {
 							storeSpawnInfo(biomeSpawns, name, rarity, type, b, s);
-						}else if (s == SpawnLocation.AirPersistent) {
-								storeSpawnInfo(airSpawns, name, rarity, type, b, s);
+						} else if (s == SpawnLocation.AirPersistent) {
+							storeSpawnInfo(airSpawns, name, rarity, type, b, s);
 						} else if (s == SpawnLocation.UnderGround) {
 							storeSpawnInfo(undergroundSpawns, name, rarity, type, b, s);
 						} else if (s == SpawnLocation.Water || s == SpawnLocation.OnWater) {
@@ -52,6 +54,7 @@ public class SpawnRegistry {
 				}
 			}
 		}
+
 	}
 
 	private static void storeSpawnInfo(HashMap<BiomeGenBase, List<SpawnData>> hashmap, String name, int rarity, ClassType type, BiomeGenBase b, SpawnLocation s) {
@@ -68,30 +71,46 @@ public class SpawnRegistry {
 	public static void getGenerationInfo(HashMap<String, String> hashmap, String name) {
 		List<SpawnData> spawnList = new ArrayList<SpawnData>();
 		int ID = Entity3HasStats.getBaseStats(name).nationalPokedexNumber;
+		ClassType type = ClassType.Pixelmon;
+		int rare = Entity3HasStats.getBaseStats(name).rarity;
 		if (!hashmap.containsKey(name)) {
-			if (ID <= 151) {
-			//	System.out.println("Generation 1: " + name);
-				hashmap.put(name, "Gen1");
-			} else if (ID > 151 && ID <= 251) {
-				//System.out.println("Generation 2: " + name);
-				hashmap.put(name, "Gen2");
-			} else if (ID > 251 && ID <= 386) {
-			//	System.out.println("Generation 3: " + name);
-				hashmap.put(name, "Gen3");
-			} else if (ID > 386 && ID <= 493) {
-			//	System.out.println("Generation 4: " + name);
-				hashmap.put(name, "Gen4");
-			} else if (ID > 493 && ID <= 649) {
-			//	System.out.println("Generation 5: " + name);
-				hashmap.put(name, "Gen5");
-			}else{
-				System.out.println("[Pixelmon]" + name + " does not have a valid id number");
-			}
+			if (rare > 0)
+				if (ID <= 151) {
+					if (PixelmonConfig.Gen1 == true) {
+						//System.out.println("Generation 1: " + name);
+						SpawnRegistry.addSpawn(name, rare, type);
+						hashmap.put(name, "Gen1");
+					}
+				} else if (ID > 151 && ID <= 251) {
+					if (PixelmonConfig.Gen2 == true) {
+						//System.out.println("Generation 2: " + name);
+						SpawnRegistry.addSpawn(name, rare, type);
+						hashmap.put(name, "Gen2");
+					}
+				} else if (ID > 251 && ID <= 386) {
+					if (PixelmonConfig.Gen3 == true) {
+						//System.out.println("Generation 3: " + name);
+						SpawnRegistry.addSpawn(name, rare, type);
+						hashmap.put(name, "Gen3");
+					}
+				} else if (ID > 386 && ID <= 493) {
+					if (PixelmonConfig.Gen4 == true) {
+						//System.out.println("Generation 4: " + name);
+						SpawnRegistry.addSpawn(name, rare, type);
+						hashmap.put(name, "Gen4");
+					}
+				} else if (ID > 493 && ID <= 649) {
+					if (PixelmonConfig.Gen5 == true) {
+						//System.out.println("Generation 5: " + name);
+						SpawnRegistry.addSpawn(name, rare, type);
+						hashmap.put(name, "Gen5");
+					}
+				} else {
+					System.out.println("[Pixelmon]" + name + " does not have a valid id number");
+				}
 		}
 
 	}
-
-	// }
 
 	public static List<SpawnData> getSpawnsForBiome(BiomeGenBase b) {
 		return biomeSpawns.get(b);
