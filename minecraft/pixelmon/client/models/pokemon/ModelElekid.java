9686764edc91074d52a675dfb6469ba520609c8e
Xwaffle1 <bucket123456745@yahoo.com>
2013-09-28 16:16:01 -0400

    Added Elekid.

---
+++ minecraft/pixelmon/client/models/pokemon/ModelElekid.java
@@ -0,0 +1,82 @@
+package pixelmon.client.models.pokemon;
+
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+import pixelmon.client.models.ModelOBJWrapper;
+import pixelmon.client.models.PixelmonModelBase;
+import pixelmon.client.models.PixelmonModelRenderer;
+import pixelmon.client.models.animations.EnumArm;
+import pixelmon.client.models.animations.EnumLeg;
+import pixelmon.client.models.animations.EnumPhase;
+import pixelmon.client.models.animations.ModuleArm;
+import pixelmon.client.models.animations.ModuleHead;
+import pixelmon.client.models.animations.ModuleLeg;
+import pixelmon.client.models.animations.ModuleTail;
+import pixelmon.client.models.animations.ModuleTailBasic;
+import pixelmon.client.models.animations.SkeletonBase;
+import pixelmon.client.models.animations.biped.SkeletonBiped;
+
+public class ModelElekid extends PixelmonModelBase {
+
+	PixelmonModelRenderer Body, Larm, Rarm, Lleg, RLeg, tail;
+
+	public ModelElekid() {
+		textureWidth = 64;
+		textureHeight = 32;
+		Body = new PixelmonModelRenderer(this, "Body");
+		Body.setRotationPoint(0, 24, 0);
+		Body.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/elekid/Body.obj")));
+
+		Larm = new PixelmonModelRenderer(this, 0, 0);
+		Larm.setRotationPoint(1.2F, 2.319F, 0.05F);
+		Larm.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/elekid/LeftArm.obj")));
+
+		Rarm = new PixelmonModelRenderer(this, 0, 0);
+		Rarm.setRotationPoint(-0.87F, 2.319F, 0.05F);
+		Rarm.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/elekid/RightArm.obj")));
+
+		Lleg = new PixelmonModelRenderer(this, 0, 0);
+		Lleg.setRotationPoint(0.613F, 1.07F, 0);
+		Lleg.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/elekid/LeftLeg.obj")));
+
+		RLeg = new PixelmonModelRenderer(this, 0, 0);
+		RLeg.setRotationPoint(-0.299F, 1.07F, 0);
+		RLeg.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/elekid/RightLeg.obj")));
+
+		Body.addChild(Larm);
+		Body.addChild(Rarm);
+		Body.addChild(Lleg);
+		Body.addChild(RLeg);
+
+		int degrees = 180;
+		float radians = (float) Math.toRadians(degrees);
+
+		setRotation(Body, radians, 0, 0);
+		float legspeed = 0.5F;
+		float legRotationLimit = 0.8F;
+
+		ModuleArm leftArm = new ModuleArm(Larm, EnumArm.Left, 0.3F, 0.8F, legspeed);
+		ModuleArm rightArm = new ModuleArm(Rarm, EnumArm.Right, 0.3F, 0.8F, legspeed);
+		ModuleLeg leftLeg = new ModuleLeg(Lleg, EnumLeg.FrontLeft, EnumPhase.InPhase, legRotationLimit, legspeed);
+		ModuleLeg rightLeg = new ModuleLeg(RLeg, EnumLeg.FrontRight, EnumPhase.InPhase, legRotationLimit, legspeed);
+
+		skeleton = new SkeletonBiped(Body, null, leftArm, rightArm, leftLeg, rightLeg, null);
+	}
+
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		super.render(entity, f, f1, f2, f3, f4, f5);
+		setRotationAngles(f, f1, f2, f3, f4, f5);
+		Body.render(f5);
+	}
+
+	private void setRotation(PixelmonModelRenderer model, float x, float y, float z) {
+		model.rotateAngleX = x;
+		model.rotateAngleY = y;
+		model.rotateAngleZ = z;
+	}
+
+	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5) {
+	}
+}
