ae64d677b9caa044f46bfdae5cd3f9c862cce8f3
Xwaffle1 <chasecool56@gmail.com>
2013-09-29 14:44:18 -0400

    Added Drifloon and Drifblim

---
+++ minecraft/pixelmon/client/models/pokemon/ModelDrifloon.java
@@ -0,0 +1,53 @@
+package pixelmon.client.models.pokemon;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+import pixelmon.client.models.ModelOBJWrapper;
+import pixelmon.client.models.PixelmonModelBase;
+import pixelmon.client.models.PixelmonModelRenderer;
+import pixelmon.client.models.animations.EnumArm;
+import pixelmon.client.models.animations.EnumLeg;
+import pixelmon.client.models.animations.EnumPhase;
+import pixelmon.client.models.animations.ModuleArm;
+import pixelmon.client.models.animations.ModuleHead;
+import pixelmon.client.models.animations.ModuleLeg;
+import pixelmon.client.models.animations.ModuleTailBasic;
+import pixelmon.client.models.animations.SkeletonBase;
+import pixelmon.client.models.animations.biped.SkeletonBiped;
+import pixelmon.client.models.animations.quadruped.SkeletonQuadruped;
+
+public class ModelDrifloon extends PixelmonModelBase {
+
+	PixelmonModelRenderer Body;
+
+	public ModelDrifloon() {
+		textureWidth = 64;
+		textureHeight = 32;
+		Body = new PixelmonModelRenderer(this, "Body");
+		Body.setRotationPoint(0, 21, 0);
+		Body.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/drifloon/Body.obj")));
+
+
+		float legspeed = 0.5F;
+		float legRotationLimit = 1.4F;
+
+		skeleton = new SkeletonBase(Body);
+	}
+
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		super.render(entity, f, f1, f2, f3, f4, f5);
+		setRotationAngles(f, f1, f2, f3, f4, f5);
+		Body.render(f5);
+	}
+
+	private void setRotation(PixelmonModelRenderer model, float x, float y, float z) {
+		model.rotateAngleX = x;
+		model.rotateAngleY = y;
+		model.rotateAngleZ = z;
+	}
+
+	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5) {
+	}
+}
