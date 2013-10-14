a5aa2445b852e50c14502a099e96088aa9eb3c97
MrMasochism <malc.geddes@gmail.com>
2013-10-15 07:39:43 +1300

    Fixed weird behaviour when riding a pokemon which didn't have offsets set

--- minecraft/pixelmon/entities/pixelmon/stats/RidingOffsets.java
+++ minecraft/pixelmon/entities/pixelmon/stats/RidingOffsets.java
@@ -6,15 +6,18 @@ public class RidingOffsets {
 	public Vector3f standing;
 	public Vector3f moving;
 
-	public void setStandingOffsets(double x, double y, double z) {
+	public RidingOffsets(){
 		standing = new Vector3f();
+		moving = new Vector3f();
+	}
+	
+	public void setStandingOffsets(double x, double y, double z) {
 		standing.x = (float)x;
 		standing.y = (float)y;
 		standing.z = (float)z;
 	}
 
 	public void setMovingOffsets(float x, float y, float z) {
-		moving = new Vector3f();
 		moving.x = (float)x;
 		moving.y = (float)y;
 		moving.z = (float)z;
