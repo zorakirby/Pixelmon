e2e3b25a0cb1548ce139deba2195a238e5583a96
Xwaffle1 <chasecool56@gmail.com>
2013-10-14 22:27:35 -0400

    Magikarp dies on land again.

--- minecraft/pixelmon/client/models/pokemon/ModelMagikarp.java
+++ minecraft/pixelmon/client/models/pokemon/ModelMagikarp.java
@@ -1,5 +1,5 @@
 package pixelmon.client.models.pokemon;
- 
+
 import net.minecraft.entity.Entity;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
@@ -15,45 +15,48 @@ import pixelmon.client.models.animations.ModuleLeg;
 import pixelmon.client.models.animations.ModuleTailBasic;
 import pixelmon.client.models.animations.SkeletonBase;
 import pixelmon.client.models.animations.biped.SkeletonBiped;
- 
-public class ModelMagikarp extends PixelmonModelBase
-{
-    //fields
- 
-    PixelmonModelRenderer Body;
- 
-    public ModelMagikarp()
-    {
-        textureWidth = 64;
-        textureHeight = 32;
-        Body = new PixelmonModelRenderer(this, "Body");
-        Body.setRotationPoint(0, 22, 0);
-        Body.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/magikarp/Body.obj")));
-       
-        int degrees = 180;
-        float radians = (float) Math.toRadians(degrees);
-        
-        setRotation(Body, radians, 0, 0);
-        
-        skeleton = new SkeletonBase(Body);
-    }
- 
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-    {
-        super.render(entity, f, f1, f2, f3, f4, f5);
-        setRotationAngles(f, f1, f2, f3, f4, f5);
-        Body.render(f5);
-        Body.setRotationPoint(0, 20, 0);
-    }
- 
-    private void setRotation(PixelmonModelRenderer model, float x, float y, float z)
-    {
-        model.rotateAngleX = x;
-        model.rotateAngleY = y;
-        model.rotateAngleZ = z;
-    }
- 
-    public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
-    {
-    }
+
+public class ModelMagikarp extends PixelmonModelBase {
+	// fields
+
+	PixelmonModelRenderer Body;
+
+	public ModelMagikarp() {
+		textureWidth = 64;
+		textureHeight = 32;
+		Body = new PixelmonModelRenderer(this, "Body");
+		Body.setRotationPoint(0, 22, 0);
+		Body.addOBJModel(new ModelOBJWrapper(AdvancedModelLoader.loadModel("/pixelmon/client/models/objFiles/magikarp/Body.obj")));
+
+		skeleton = new SkeletonBase(Body);
+	}
+
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		super.render(entity, f, f1, f2, f3, f4, f5);
+		if (entity.isInWater())
+			setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+		else
+			setRotationAnglesOutOfWater(f, f1, f2, f3, f4, f5);
+		Body.render(f5);
+	}
+
+	private void setRotationAnglesOutOfWater(float f, float f1, float f2, float f3, float f4, float f5) {
+		Body.rotateAngleZ = 1.5F;
+		Body.rotationPointY = 22F;
+	}
+
+	private void setRotation(PixelmonModelRenderer model, float x, float y, float z) {
+		model.rotateAngleX = x;
+		model.rotateAngleY = y;
+		model.rotateAngleZ = z;
+	}
+
+	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
+		int degrees = 180;
+		float radians = (float) Math.toRadians(degrees);
+		setRotation(Body, radians, 0, 0);
+		Body.rotateAngleZ = 0F;
+		Body.rotationPointY = 17.5F;
+	}
+
 }
