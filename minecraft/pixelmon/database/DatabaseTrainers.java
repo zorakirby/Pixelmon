483cc3aac9c7da369e4437f863bacf24d22b2664
Xwaffle1 <chasecool56@gmail.com>
2013-10-25 19:39:22 -0400

    Commiting datbase. (Makes me)

--- minecraft/pixelmon/database/DatabaseTrainers.java
+++ minecraft/pixelmon/database/DatabaseTrainers.java
@@ -12,75 +12,75 @@ import pixelmon.enums.EnumBiomes;
 import pixelmon.enums.EnumPokemon;
 
 public class DatabaseTrainers {
-	public static TrainerInfo GetTrainerInfo(String trainerName) {
-		Connection conn = null;
-		try {
-			Class.forName("org.sqlite.JDBC");
-			conn = DatabaseHelper.getConnection();
-			Statement stat = conn.createStatement();
-			ResultSet rs = stat.executeQuery("select * from Trainers where TrainerType='" + trainerName + "'");
+	 public static TrainerInfo GetTrainerInfo(String trainerName) {
+         Connection conn = null;
+         try {
+                 Class.forName("org.sqlite.JDBC");
+                 conn = DatabaseHelper.getConnection();
+                 Statement stat = conn.createStatement();
+                 ResultSet rs = stat.executeQuery("select * from Trainers where TrainerType='" + trainerName + "'");
 
-			TrainerInfo info = new TrainerInfo();
-			Random rand = new Random();
-			while (rs.next()) {
-				String nameListString = rs.getString("NameList");
-				String[] nameListSplits = nameListString.split(";");
-				info.name = nameListSplits[rand.nextInt(nameListSplits.length)];
-				String pokemonListString = rs.getString("UsablePokemon");
-				String[] pokemonListSplits = pokemonListString.split(";");
-				ArrayList<String> newList = new ArrayList<String>();
-				for (String s : pokemonListSplits)
-					if (EnumPokemon.hasPokemon(s))
-						newList.add(s);
+                 TrainerInfo info = new TrainerInfo();
+                 Random rand = new Random();
+                 while (rs.next()) {
+                         String nameListString = rs.getString("NameList");
+                         String[] nameListSplits = nameListString.split(";");
+                         info.name = nameListSplits[rand.nextInt(nameListSplits.length)];
+                         String pokemonListString = rs.getString("UsablePokemon");
+                         String[] pokemonListSplits = pokemonListString.split(";");
+                         ArrayList<String> newList = new ArrayList<String>();
+                         for (String s : pokemonListSplits)
+                                 if (EnumPokemon.hasPokemon(s))
+                                         newList.add(s);
 
-				int numPokemon = rand.nextInt(6) + 1;
-				for (int i = 0; i < numPokemon; i++) {
-					info.partypokemon.add(newList.get(rand.nextInt(newList.size())));
-				}
-				int baseLevel = rs.getInt("BaseLevel");
-				int topLevel = rs.getInt("TopLevel");
-				info.level = baseLevel + rand.nextInt(topLevel + 1 - baseLevel);
-				String greetingListString = rs.getString("Greetings");
-				String[] greetingListSplits = greetingListString.split(";");
-				int ind = rand.nextInt(greetingListSplits.length);
-				info.greeting = greetingListSplits[ind];
-				String winningListString = rs.getString("VictoryMessages");
-				String defeatListString = rs.getString("DefeatMessages");
-				if (winningListString == null)
-					info.winMessage = "NO WINNING MESSAGE!";
-				else {
-					String[] winningListSplits = winningListString.split(";");
-					if (winningListSplits.length < ind - 1)
-						info.winMessage = "NO WINNING MESSAGE!";
-					else
-						info.winMessage = winningListSplits[ind];
-				}
-				if (defeatListString == null)
-					info.loseMessage = "NO DEFEAT MESSAGE!";
-				else {
-					String[] defeatListSplits = defeatListString.split(";");
-					if (defeatListSplits.length < ind - 1)
-						info.loseMessage = "NO DEFEAT MESSAGE!";
-					else
-						info.loseMessage = defeatListSplits[ind];
-				}
-				info.rarity = rs.getInt("Rarity");
-				String modelStrings = rs.getString("Models");
-				String[] modelSplits = modelStrings.split(";");
-				info.model = modelSplits[rand.nextInt(modelSplits.length)];
-			}
-			conn.close();
-			return info;
-		} catch (Exception e) {
-			if (conn != null)
-				try {
-					conn.close();
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-		}
-		return null;
-	}
+                         int numPokemon = rand.nextInt(6) + 1;
+                         for (int i = 0; i < numPokemon; i++) {
+                                 info.partypokemon.add(newList.get(rand.nextInt(newList.size())));
+                         }
+                         int baseLevel = rs.getInt("BaseLevel");
+                         int topLevel = rs.getInt("TopLevel");
+                         info.level = baseLevel + rand.nextInt(topLevel + 1 - baseLevel);
+                         String greetingListString = rs.getString("Greetings");
+                         String[] greetingListSplits = greetingListString.split(";");
+                         int ind = rand.nextInt(greetingListSplits.length);
+                         info.greeting = greetingListSplits[ind];
+                         String winningListString = rs.getString("VictoryMessages");
+                         String defeatListString = rs.getString("DefeatMessages");
+                         if (winningListString == null)
+                                 info.winMessage = "NO WINNING MESSAGE!";
+                         else {
+                                 String[] winningListSplits = winningListString.split(";");
+                                 if (winningListSplits.length < ind - 1)
+                                         info.winMessage = "NO WINNING MESSAGE!";
+                                 else
+                                         info.winMessage = winningListSplits[ind];
+                         }
+                         if (defeatListString == null)
+                                 info.loseMessage = "NO DEFEAT MESSAGE!";
+                         else {
+                                 String[] defeatListSplits = defeatListString.split(";");
+                                 if (defeatListSplits.length < ind - 1)
+                                         info.loseMessage = "NO DEFEAT MESSAGE!";
+                                 else
+                                         info.loseMessage = defeatListSplits[ind];
+                         }
+                         info.rarity = rs.getInt("Rarity");
+                         String modelStrings = rs.getString("Models");
+                         String[] modelSplits = modelStrings.split(";");
+                         info.model = modelSplits[rand.nextInt(modelSplits.length)];
+                 }
+                 conn.close();
+                 return info;
+         } catch (Exception e) {
+                 if (conn != null)
+                         try {
+                                 conn.close();
+                         } catch (SQLException e1) {
+                                 e1.printStackTrace();
+                         }
+         }
+         return null;
+ }
 
 	public static BiomeGenBase[] GetSpawnBiomes(String trainerName) {
 		Connection conn = null;
